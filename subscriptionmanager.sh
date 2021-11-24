5a18d1229a9868a4b024516500ea8f9ca49f9df1
Jakob Holst <jho@miracle.dk>
2021-11-24 07:17:11 +0100

    working on prettying spec

--- subscriptionmanager.sh
+++ subscriptionmanager.sh
@@ -16,13 +16,19 @@ else
 fi
 
 #Connect and get the access token
+curl https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$offline_token 
 curl https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$offline_token > access.token 2>/dev/null
 access_token=`cat access.token |jq '.access_token'|tr -d '"'`
+
+echo $access_token
 
 
 curl -H "Authorization: Bearer $access_token" "https://api.access.redhat.com/management/v1/subscriptions"  >subscriptions.json 2>/dev/null
 curl -H "Authorization: Bearer $access_token" "https://api.access.redhat.com/management/v1/systems"  >systems.json 2>/dev/null
 # leave benny and delete the rest :-) 
+
+
+cat systems.json 
 for UUID in `cat systems.json  |jq '.body[] | "\(.name) \(.uuid)"'|tr -d '"'|grep -vf  ignore.systems |awk '{ print $2 }'`
 do
 	echo "`date`: Delete system : $UUID"
